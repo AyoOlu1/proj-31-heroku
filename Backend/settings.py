d00b17d53b047384f120f246764402bcd95a7136
Victor Oluwayemi <72816381+AyoOlu1@users.noreply.github.com>
2022-08-17 23:57:12 +0100

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$07nqm_y^$7#8s688sn4qrm%7h38w^*t_4qqrglo8%zf34_mn9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
